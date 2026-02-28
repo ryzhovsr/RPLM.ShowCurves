fec7ff0e90f826efb286cf1841d59a2849313e2b
NSPodmarev <NSPodmarev@yandex.ru>
2026-02-28 15:29:04 +0300

    Temp

--- RPLM.ShowCurves/src/Include/RPLM.CAD.UI.ConjugationCurves.Resources.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-namespace RPLM::CAD::UI
-{
-	const RPLM::Base::Framework::String& GetModuleName();
-}
-
-#define RSCADUIW(key)	RPLM::Base::Framework::GetModuleResource(L##key, RPLM::CAD::UI::GetModuleName())
